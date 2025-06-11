9812805371f90eeefafc680656231f2686db89bb
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-06-11 14:00:23 +0000

    shorter file headers

--- src/boot3/prog.h
+++ src/boot3/prog.h
@@ -1,16 +1,6 @@
 /**
- * BSD Zero Clause License (SPDX: 0BSD)
- * 
- * Permission to use, copy, modify, and/or distribute this software for any purpose
- * with or without fee is hereby granted.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
- * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
- * THIS SOFTWARE.
+ * https://github.com/DLehenbauer/pico-sdcard-bootloader
+ * SPDX-License-Identifier: 0BSD
  */
 
 #pragma once
@@ -37,11 +27,11 @@ typedef struct prog_s prog_t;
 typedef bool (*accept_block_cb_t)(prog_t* prog, const struct uf2_block* block);
 
 typedef struct prog_s {
-    interval_set_t pages_written;           // The set of pages to be written when flashing the UF2 file.
-    interval_set_t sectors_erased;          // The set of sectors to be erased before flashing the UF2 file.
-    uint32_t num_blocks;                    // Total number of blocks in UF2 file
-    uint32_t num_blocks_accepted;           // Number of blocks received so far
-    accept_block_cb_t accept_block;         // Invoked for each valid program block received.
+    interval_set_t pages_written;           // Tracks which flash pages have been written to detect overlapping writes.
+    interval_set_t sectors_erased;          // Tracks which flash sectors have been written for bulk erasure.
+    uint32_t num_blocks;                    // Total number of blocks declared in the UF2 file
+    uint32_t num_blocks_accepted;           // Number of blocks accepted for writing so far.
+    accept_block_cb_t accept_block;         // Invoked for each valid program block that is accepted for writing.
     uint8_t vector_table[FLASH_PAGE_SIZE];  // Pending vector table to write at the end of the programming process
     bool has_vector_table;                  // True if the vector table was found in the UF2 file
     bool is_different;                      // True if the UF2 file differs from the current flash contents
