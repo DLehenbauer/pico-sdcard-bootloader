bf6890f48ca60db7456c52b1cfcffcf2631d1dfc
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2025-06-11 13:59:17 +0000

    get stage2 bootloader from SDK

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 set -e
 
 rm -rf build
+rm ./dist/bootloader.uf2 || true
+
 cmake --preset rp2040_release
 cmake --build --preset rp2040_release
 
 mkdir -p dist
-cp /workspaces/rp2040-sdcard-bootloader/build/rp2040_release/src/boot3/bootloader.uf2 dist/sd_bootloader_v1.0.uf2
+cp /workspaces/rp2040-sdcard-bootloader/build/rp2040_release/src/boot3/bootloader.uf2 ./dist
